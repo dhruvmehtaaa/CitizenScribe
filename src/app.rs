7c2f27612cac1a9c7d15e9780649dcdfebf171e0
dhruvmehtaaa <dhruvmehtaalt@gmail.com>
2023-09-30 16:13:15 +0530

    Created a message struct and a signal that returns a tuple

--- src/app.rs
+++ src/app.rs
@@ -2,16 +2,32 @@ use leptos::*;
 use leptos_meta::*;
 use leptos_router::*;
 
+use crate::model::conversation::{Conversation, Message};
+
 #[component]
 pub fn App(cx: Scope) -> impl IntoView {
     provide_meta_context(cx);
 
+    let (conversation,set_conversation)= create signal(cx, Conversation::new());
+
+    let send = create_action(move |new_message: &String| {
+        let user_message = Message {
+            text: new_message.clone(),
+            user: true,
+        };
+        set_conversation.update(move |c| {
+            c.messages.push(user_message);
+        });
+
+        //TODO
+
+    });
+
     view! { cx,
         <Stylesheet id="leptos" href="/pkg/leptos_start.css"/>
         <Title text="Citizen Scribe"/>
-        <ChatArea/>
-        <TypeArea/>
-        
+        <ChatArea conversation/>
+        <TypeArea send/>        
     }
 }
 
