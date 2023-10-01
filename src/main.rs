d24f00c5f68b519c5f8a3be9d7a3c0d2498b7624
dhruvmehtaaa <dhruvmehtaalt@gmail.com>
2023-10-01 17:05:34 +0530

    Added logic to load model and more

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,12 @@
+use cfg_if::cfg_if;
+
 #[cfg(feature = "ssr")]
 #[actix_web::main]
 async fn main() -> std::io::Result<()> {
+    // debug logging, disable for prod
+    std::env::set_var("RUST_LOG", "debug");
+    env_logger::init();
+
     use actix_files::Files;
     use actix_web::*;
     use leptos::*;
@@ -12,56 +18,68 @@ async fn main() -> std::io::Result<()> {
     // Generate the list of routes in your Leptos App
     let routes = generate_route_list(|cx| view! { cx, <App/> });
 
+    #[get("/style.css")]
+    async fn css() -> impl Responder {
+        actix_files::NamedFile::open_async("./style/output.css").await
+    }
+
+    let model = web::Data::new(get_language_model());
     HttpServer::new(move || {
         let leptos_options = &conf.leptos_options;
         let site_root = &leptos_options.site_root;
 
         App::new()
+            .app_data(model.clone())
+            .service(css)
             .route("/api/{tail:.*}", leptos_actix::handle_server_fns())
-            // serve JS/WASM/CSS from `pkg`
-            .service(Files::new("/pkg", format!("{site_root}/pkg")))
-            // serve other assets from the `assets` directory
-            .service(Files::new("/assets", site_root))
-            // serve the favicon from /favicon.ico
-            .service(favicon)
             .leptos_routes(
                 leptos_options.to_owned(),
                 routes.to_owned(),
                 |cx| view! { cx, <App/> },
             )
-            .app_data(web::Data::new(leptos_options.to_owned()))
-        //.wrap(middleware::Compress::default())
+            .service(Files::new("/", site_root))
     })
     .bind(&addr)?
     .run()
     .await
 }
 
-#[cfg(feature = "ssr")]
-#[actix_web::get("favicon.ico")]
-async fn favicon(
-    leptos_options: actix_web::web::Data<leptos::LeptosOptions>,
-) -> actix_web::Result<actix_files::NamedFile> {
-    let leptos_options = leptos_options.into_inner();
-    let site_root = &leptos_options.site_root;
-    Ok(actix_files::NamedFile::open(format!(
-        "{site_root}/favicon.ico"
-    ))?)
+cfg_if! {
+    if #[cfg(feature = "ssr")] {
+        use llm::models::Llama;
+        use actix_web::*;
+        use std::env;
+        use dotenv::dotenv;
+        fn get_language_model() -> Llama {
+            use std::path::PathBuf;
+            dotenv().ok();
+            let model_path = env::var("MODEL_PATH").expect("MODEL_PATH must be set");
+            let model_parameters = llm::ModelParameters {
+                prefer_mmap: true,
+                context_size: 2048,
+                lora_adapters: None,
+                use_gpu: true,
+            };
+
+            llm::load::<Llama>(
+                &PathBuf::from(&model_path),
+                llm::TokenizerSource::Embedded,
+                model_parameters,
+                llm::load_progress_callback_stdout,
+            )
+            .unwrap_or_else(|err| {
+                panic!("Failed to load model from {model_path:?}: {err}")
+            })
+        }
+    }
 }
 
 #[cfg(not(any(feature = "ssr", feature = "csr")))]
 pub fn main() {
-    // no client-side main function
-    // unless we want this to work with e.g., Trunk for pure client-side testing
-    // see lib.rs for hydration function instead
-    // see optional feature `csr` instead
 }
 
 #[cfg(all(not(feature = "ssr"), feature = "csr"))]
 pub fn main() {
-    // a client-side main function is required for using `trunk serve`
-    // prefer using `cargo leptos serve` instead
-    // to run: `trunk serve --open --features csr`
     use leptos::*;
     use leptos_start::app::*;
     use wasm_bindgen::prelude::wasm_bindgen;
@@ -69,8 +87,6 @@ pub fn main() {
     console_error_panic_hook::set_once();
 
     leptos::mount_to_body(move |cx| {
-        // note: for testing it may be preferrable to replace this with a
-        // more specific component, although leptos_router should still work
         view! {cx, <App/> }
     });
 }
