b1a8a211081f7058ed2c3f1d62a44a9390fa5dd5
dhruvmehtaaa <dhruvmehtaalt@gmail.com>
2023-10-05 13:25:49 +0530

    Update Readme

--- src/model/conversation.rs
+++ src/model/conversation.rs
@@ -2,7 +2,7 @@ use serde::Deserialize;
 use serde::Serialize;
 
 #[derive(Serialize, Deserialize, Clone, Debug)]
-pub struct Conversation{
+pub struct Conversation {
     pub messages: Vec<Message>
 }
 
